ed0c6ff38994f1998d87eb7a924816a7b64c7ac1
Vincent Rabaud <vrabaud@willowgarage.com>
2011-10-25 03:47:38 +0000

    Revert to pre newer boost atomic until boost atomic is fixed

--- rosatomic/boost_atomic/atomic/detail/generic-cas.hpp
+++ rosatomic/boost_atomic/atomic/detail/generic-cas.hpp
@@ -1,5 +1,5 @@
-#ifndef BOOST_DETAIL_ATOMIC_GENERIC_CAS_HPP
-#define BOOST_DETAIL_ATOMIC_GENERIC_CAS_HPP
+#ifndef BOOST_ATOMIC_DETAIL_ATOMIC_GENERIC_CAS_HPP
+#define BOOST_ATOMIC_DETAIL_ATOMIC_GENERIC_CAS_HPP
 
 //  Copyright (c) 2009 Helge Bahmann
 //
@@ -9,9 +9,9 @@
 
 #include <stdint.h>
 
-#include <boost/memory_order.hpp>
-#include <boost/atomic/detail/base.hpp>
-#include <boost/atomic/detail/builder.hpp>
+#include "../memory_order2.hpp"
+#include "base.hpp"
+#include "builder.hpp"
 
 /* fallback implementation for various compilation targets;
 this is *not* efficient, particularly because all operations
@@ -19,14 +19,15 @@ are fully fenced (full memory barriers before and after
 each operation) */
 
 #if defined(__GNUC__)
-	namespace boost { namespace detail { namespace atomic {
+	namespace boost_atomic { namespace detail { namespace atomic {
+        using namespace boost;
 	static inline int32_t
 	fenced_compare_exchange_strong_32(volatile int32_t *ptr, int32_t expected, int32_t desired)
 	{
 		return __sync_val_compare_and_swap_4(ptr, expected, desired);
 	}
 	#define BOOST_ATOMIC_HAVE_CAS32 1
-	
+
 	#if defined(__amd64__) || defined(__i686__)
 	static inline int64_t
 	fenced_compare_exchange_strong_64(int64_t *ptr, int64_t expected, int64_t desired)
@@ -44,7 +45,7 @@ each operation) */
 	#include <intrin.h>
 	#endif
 
-	namespace boost { namespace detail { namespace atomic {
+	namespace boost_atomic { namespace detail { namespace atomic {
 	static inline int32_t
 	fenced_compare_exchange_strong(int32_t *ptr, int32_t expected, int32_t desired)
 	{
@@ -60,9 +61,9 @@ each operation) */
 	#define BOOST_ATOMIC_HAVE_CAS64 1
 	#endif
 	}}}
-	
+
 #elif (defined(__ICC) || defined(__ECC))
-	namespace boost { namespace detail { namespace atomic {
+	namespace boost_atomic { namespace detail { namespace atomic {
 	static inline int32_t
 	fenced_compare_exchange_strong_32(int32_t *ptr, int32_t expected, int32_t desired)
 	{
@@ -85,7 +86,7 @@ each operation) */
 	#define BOOST_ATOMIC_HAVE_CAS64 1
 	#endif
 	}}}
-	
+
 #elif (defined(__SUNPRO_CC) && defined(__sparc))
 	#include <sys/atomic.h>
 	namespace boost { namespace detail { namespace atomic {
@@ -95,7 +96,7 @@ each operation) */
 		return atomic_cas_32((volatile unsigned int*)ptr, expected, desired);
 	}
 	#define BOOST_ATOMIC_HAVE_CAS32 1
-	
+
 	/* FIXME: check for 64 bit mode */
 	static inline int64_t
 	fenced_compare_exchange_strong_64(int64_t *ptr, int64_t expected, int64_t desired)
@@ -107,7 +108,7 @@ each operation) */
 #endif
 
 
-namespace boost { namespace detail { namespace atomic {
+namespace boost_atomic { namespace detail { namespace atomic {
 
 #ifdef BOOST_ATOMIC_HAVE_CAS32
 template<typename T>
@@ -117,21 +118,21 @@ private:
 public:
 	explicit atomic_generic_cas32(T v) : i((int32_t)v) {}
 	atomic_generic_cas32() {}
-	T load(memory_order order=memory_order_seq_cst) const volatile
+	T load(memory_order2 order=memory_order2_seq_cst) const volatile
 	{
 		T expected=(T)i;
-		do { } while(!const_cast<this_type *>(this)->compare_exchange_weak(expected, expected, order, memory_order_relaxed));
+		do { } while(!const_cast<this_type *>(this)->compare_exchange_weak(expected, expected, order, memory_order2_relaxed));
 		return expected;
 	}
-	void store(T v, memory_order order=memory_order_seq_cst) volatile
+	void store(T v, memory_order2 order=memory_order2_seq_cst) volatile
 	{
 		exchange(v);
 	}
 	bool compare_exchange_strong(
 		T &expected,
 		T desired,
-		memory_order success_order,
-		memory_order failure_order) volatile
+		memory_order2 success_order,
+		memory_order2 failure_order) volatile
 	{
 		T found;
 		found=(T)fenced_compare_exchange_strong_32(&i, (int32_t)expected, (int32_t)desired);
@@ -142,18 +143,18 @@ public:
 	bool compare_exchange_weak(
 		T &expected,
 		T desired,
-		memory_order success_order,
-		memory_order failure_order) volatile
+		memory_order2 success_order,
+		memory_order2 failure_order) volatile
 	{
 		return compare_exchange_strong(expected, desired, success_order, failure_order);
 	}
-	T exchange(T r, memory_order order=memory_order_seq_cst) volatile
+	T exchange(T r, memory_order2 order=memory_order2_seq_cst) volatile
 	{
 		T expected=(T)i;
-		do { } while(!compare_exchange_weak(expected, r, order, memory_order_relaxed));
+		do { } while(!compare_exchange_weak(expected, r, order, memory_order2_relaxed));
 		return expected;
 	}
-	
+
 	bool is_lock_free(void) const volatile {return true;}
 	typedef T integral_type;
 private:
@@ -172,7 +173,7 @@ template<typename T>
 class platform_atomic_integral<T, 1>: public build_atomic_from_larger_type<atomic_generic_cas32<int32_t>, T> {
 public:
 	typedef build_atomic_from_larger_type<atomic_generic_cas32<int32_t>, T> super;
-	
+
 	explicit platform_atomic_integral(T v) : super(v) {}
 	platform_atomic_integral(void) {}
 };
@@ -181,7 +182,7 @@ template<typename T>
 class platform_atomic_integral<T, 2>: public build_atomic_from_larger_type<atomic_generic_cas32<int32_t>, T> {
 public:
 	typedef build_atomic_from_larger_type<atomic_generic_cas32<int32_t>, T> super;
-	
+
 	explicit platform_atomic_integral(T v) : super(v) {}
 	platform_atomic_integral(void) {}
 };
